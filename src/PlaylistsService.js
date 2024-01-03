33d0ecf7e807e6c9bec8aaec7cd4ea98f49e1983
rasyadpras <106673670+rasyadpras@users.noreply.github.com>
2024-01-03 22:54:23 +0700

    first

---
+++ src/PlaylistsService.js
@@ -0,0 +1,36 @@
+const { Pool } = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylistSong(playlistId) {
+    const query = {
+      text: 'SELECT id, name FROM playlists WHERE id = $1',
+      values: [playlistId],
+    };
+
+    const songQuery = {
+      text: `SELECT songs.id, songs.title, songs.performer FROM playlists
+      JOIN playlist_songs ON playlist_songs.playlist_id = playlists.id
+      JOIN songs ON songs.id = playlist_songs.song_id 
+      WHERE playlists.id = $1`,
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+    const resultSongs = await this._pool.query(songQuery);
+
+    const playlist = result.rows[0];
+    const exportResult = {
+      id: playlist.id,
+      name: playlist.name,
+      songs: resultSongs.rows,
+    };
+
+    return exportResult;
+  }
+}
+
+module.exports = PlaylistsService;
